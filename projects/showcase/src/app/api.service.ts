803e98a63c30b4dd725975ea2e6ec8fafbb9bdd5
Adam Kariv <adam.kariv@gmail.com>
2025-04-20 12:27:29 +0300

    Tiles URL

--- projects/showcase/src/app/api.service.ts
+++ projects/showcase/src/app/api.service.ts
@@ -6,7 +6,7 @@ import { ReplaySubject } from "rxjs";
   providedIn: 'root'
 })
 export class ApiService {
-  URL = 'https://storage.googleapis.com/chronomaps3.firebasestorage.app/tiles/4d2c04b0-51b7-4aa2-a234-0e4be53447de/0/config.json?a=3';
+  URL = 'https://storage.googleapis.com/chronomaps3.firebasestorage.app/tiles/4d2c04b0-51b7-4aa2-a234-0e4be53447de/0/config.json?a=4';
 
   config = new ReplaySubject<any>(1);
 
