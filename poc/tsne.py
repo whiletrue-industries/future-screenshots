ecdc05ced939dec6891e865bd726cfcaeb491e71
Adam Kariv <adam.kariv@gmail.com>
2025-04-24 12:33:56 +0300

    poc

--- poc/tsne.py
+++ poc/tsne.py
@@ -1,17 +1,31 @@
 import json
+import concurrent.futures
+from io import BytesIO
 import numpy as np
 from sklearn.manifold import TSNE
 from scipy.spatial.distance import cdist
 from lapjv import lapjv
 import random
+import math
+import os
 from PIL import Image
+from skimage import transform
+from scipy.ndimage import gaussian_filter
 
 EMBEDDING_DIMENSION = 3072
 PERPLEXITY = 50
 TSNE_ITER = 5000
-out_dim = (30, 15)
-TO_PLOT = 450
-side = 1000
+ORIGINAL_IMAGE_SIZE = (530, 1000)
+CELL_RATIOS = (1.86, 1.135)
+
+OUT_DIM_X = 14
+OUT_RATIO = 16/9
+OUT_DIM_Y = int(math.ceil(OUT_DIM_X * ORIGINAL_IMAGE_SIZE[0] * CELL_RATIOS[0] * OUT_RATIO / (ORIGINAL_IMAGE_SIZE[1] * CELL_RATIOS[1])))
+out_dim = (OUT_DIM_X, OUT_DIM_Y)
+print("Output dimensions:", out_dim)
+TO_PLOT = int(out_dim[0] * out_dim[1] * 0.75)
+SIDE = 1000
+PADDING = int(0.285 * SIDE)
 
 def load_records():
     with open('sample/affb/records_with_embeddings.json', 'r') as f:
@@ -32,12 +46,14 @@ def calc_tsne_grid(X_2d, out_dim):
     cost_matrix = np.hstack((cost_matrix, np.zeros((shp[0], shp[0] - shp[1]))))
     _, col_asses, _ = lapjv(cost_matrix)
     grid_jv = grid[col_asses]
+    print("Grid shape:", grid_jv.shape)
+    print("Grid", grid_jv)
     return grid_jv
 
 def get_image(filename, target_size):
     # target_size = (width, height)
     # Open the image size, resize it to the target size (maintaining aspect ratio) and return a cropped image of the target size out the center
-    inner_target_size = int(target_size[0] / 1.86), int(target_size[1] / 1.135)
+    inner_target_size = int(target_size[0] / CELL_RATIOS[0]), int(target_size[1] / CELL_RATIOS[1])
     if not filename:
         filename = 'empty-space.png'
         img = Image.open(filename)
@@ -65,20 +81,19 @@ def get_image(filename, target_size):
     out_img.paste(img, ((target_size[0] - img.width) // 2, (target_size[1] - img.height) // 2))
     return out_img
 
-def create_tsne_image(grid_jv, records, out_dim, to_plot, res, offset, out_size, padding):
+def create_tsne_image(grid_jv, records, out_dim, res, offset, padding):
     # print('>>>', filename)
     info = dict(dim=out_dim, grid=[])
 
     out_res_x, out_res_y = res
     offset_x, offset_y = offset
     padding_x, padding_y = padding
-    out_size_x, out_size_y = out_size
-    out = np.ones((out_dim[1]*out_res_y + padding_y, out_dim[0]*out_res_x + padding_x, 3), dtype=np.uint8)*255
+    out = np.ones((out_dim[1]*out_res_y + padding_y, out_dim[0]*out_res_x + padding_x, 3), dtype=np.uint8) * 255
     print("Output:", out_dim, res, out.shape, out.dtype)
     # alpha = np.zeros((img_dim*out_res_y, img_dim*out_res_x, 1))
     # used = set()
     positions = dict()
-    for pos, record in zip(grid_jv, records[0:to_plot]):
+    for pos, record in zip(grid_jv, records):
         pos_x = round(pos[1] * (out_dim[0] - 1))# + img_ofs
         pos_y = round(pos[0] * (out_dim[1] - 1))# + img_ofs
         pos = (int(pos_y), int(pos_x))
@@ -93,7 +108,7 @@ def create_tsne_image(grid_jv, records, out_dim, to_plot, res, offset, out_size,
             else:
                 filename = None
             print(f"Processing {pos}: {filename}")
-            img = get_image(filename, out_size)
+            img = get_image(filename, res)
             if callable(offset_x):
                 _offset_x = offset_x(pos_x, pos_y)
             else:
@@ -104,44 +119,130 @@ def create_tsne_image(grid_jv, records, out_dim, to_plot, res, offset, out_size,
                 _offset_y = offset_y
             h_range = pos_y * out_res_y + _offset_y
             w_range = pos_x * out_res_x + _offset_x
-            img = np.asarray(img)
+            # img = (np.asarray(img) / 255.0).astype(np.float32)
             # print("Image:", img.shape, img.dtype, out.dtype)
-            # print("Copying:", pos_y, h_range, out_size_y, '/', pos_x, w_range, out_size_x)
-            out[h_range:h_range + out_size_y, w_range:w_range + out_size_x] = img
+            # print("Copying:", pos_y, h_range, out_res_y, '/', pos_x, w_range, out_size_x)
+            out[h_range:h_range + out_res_y, w_range:w_range + out_res_x] = img
             # alpha[h_range:h_range + out_size_y, w_range:w_range + out_size_x] = 255*np.ones((out_size_y, out_size_x, 1))
             info['grid'].append(dict(pos=dict(x=pos_x, y=pos_y), item=filename))
 
     # Save out as png:
-    im = Image.fromarray(out)
+    # im = Image.fromarray(out)
     # im = image.array_to_img(out)
     # im.putalpha(image.array_to_img(alpha))
-    return im, info
+    return out, info
 
-records_with_embeddings = load_records()
 
-records, activations = records_with_embeddings, [rec['embedding'] for rec in records_with_embeddings]
+# def _create_tiles(filename, image: Image, out_dim, res, info, current_set):
+#     dim_zoom = round(math.log2(out_dim))
+#     edge = 2**math.ceil(math.log2(out_dim)) * res[0]
+#     tile_size = 256
+#     max_cut_size = tile_size * 4
+#     max_zoom = info['max_zoom'] = 8
+#     min_zoom = info['min_zoom'] = 8 - dim_zoom
 
-print("Generating 2D representation.")
-X_2d = generate_tsne(activations, TO_PLOT, PERPLEXITY, TSNE_ITER)
-print("Generating image grid (%dx%d, %d images)" % (out_dim[0], out_dim[1], len(records)))
-grid = calc_tsne_grid(X_2d, out_dim)
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+#         for zoom in range(min_zoom, max_zoom + 1):
+#             num_cuts = (2**(zoom - min_zoom))
+#             cut_size = edge / num_cuts
+#             if cut_size > max_cut_size:
+#                 scaledown_size = math.ceil((max_cut_size * num_cuts * out_dim * res[0]) / edge)
+#                 cut_size = max_cut_size
+#                 scaledown = image.resize((scaledown_size, scaledown_size), Image.NEAREST)
+#             else:
+#                 scaledown = image
 
-print("Generating image.", [x['filename'] for x in records[:10]])
-try:
-    # w, h = 530, 1000
-    w, h = 530 * 1.86, 1000 * 1.135
-    dim = max(w, h)
-    size = side #*0.9
-    size = (int(size*w/dim), int(size*h/dim))
-    res = (int(side*w/dim), int(side*h/dim))
-    offset = (0, lambda x, y: 285 * (x%2)) #(int((res[0] - size[0])/2), int((res[1] - size[1])/2))
-    padding = (0, 285)
-    image, info = create_tsne_image(grid, records, out_dim, 10000,
-                                    res, offset, size, padding)
-except Exception as e:
-    print("Error generating image:", e)
-    raise
-image.save('tsne.png')
-print('Saved image to tsne.png')
-print(info)
+#             for x in range(num_cuts):
+#                 for y in range(num_cuts):
+#                     key = f'feature-tiles/{current_set}/{filename}/{zoom}/{x}/{y}'
+#                     left = math.floor(x * cut_size)
+#                     upper = math.floor(y * cut_size)
+#                     right = math.ceil((x+1) * cut_size - 1)
+#                     lower = math.ceil((y+1) * cut_size - 1)
+#                     tile: Image = scaledown.crop((left, upper, right, lower))
+#                     tile = tile.resize((tile_size, tile_size), resample=Image.BICUBIC)
+
+#                     buff = BytesIO()
+#                     tile.save(buff, format='png', quality=90)
+#                     buff.seek(0)
+#                     executor.submit(upload_fileobj_s3, buff, key, 'image/png')
+
+def create_tiles(prefix: str, image: Image):
+    w, h = image.size
+    max_size = max(w, h)
+    tile_size = 256
+    num_tiles = math.ceil(max_size / tile_size)
+    zoom_level = math.ceil(math.log2(num_tiles))
+    num_tiles = 2**zoom_level
+    max_zoom = 8
+    min_zoom = 8 - zoom_level
+    print(f"Tiles: {prefix} ({w}x{h}) -> {num_tiles}x{num_tiles} ({tile_size}px) {zoom_level} levels")
+
+    for z in range(zoom_level):
+        zoom = max_zoom - z
+        skip = 2**z
+        # _tile_size = tile_size * skip
+        _num_tiles = num_tiles // skip
+        print(f"Zoom {zoom}: {_num_tiles}x{_num_tiles} ({tile_size}px)")
+        if skip > 1:
+            # Blur image to reduce aliasing
+            image = image.resize((w // 2, h // 2), Image.Resampling.LANCZOS)
+            # image = np.asarray(image)
+            # w, h = image.shape[1], image.shape[0]
+            w, h = image.size
+        for x in range(_num_tiles):
+            os.makedirs(f'tiles/{prefix}/{zoom}/{x}', exist_ok=True)
+            for y in range(_num_tiles):
+                # target = np.ones((_tile_size, _tile_size, 3), dtype=np.uint8) * 255
+                # target = np.ones((tile_size, tile_size, 3), dtype=np.uint8) * 255
+                target = Image.new('RGB', (tile_size, tile_size), (255, 255, 255))
+                left = min(x * tile_size, w)
+                upper = min(y * tile_size, h)
+                right = min(left + tile_size, w)
+                lower = min(upper + tile_size, h)
+                # tgt_width = len(range(left, right, skip))
+                # tgt_height = len(range(upper, lower, skip))
+                # target[:tgt_height, :tgt_width] = image[upper:lower:skip, left:right:skip]
+                # target[:lower-upper, :right-left] = image[upper:lower, left:right]
+                target.paste(image.crop((left, upper, right, lower)), (0, 0))
+                # target = transform.rescale(target, (tile_size / _tile_size, tile_size / _tile_size, 1), anti_aliasing=False)
+                # target = Image.fromarray((target * 255).astype(np.uint8))
+                # target = Image.fromarray(target)
+                target.save(f'tiles/{prefix}/{zoom}/{x}/{y}.png', format='PNG', compress_level=0)
+
+def main():
+    records_with_embeddings = load_records()
+
+    records, activations = records_with_embeddings, [rec['embedding'] for rec in records_with_embeddings]
+
+    print("Generating 2D representation.")
+    X_2d = generate_tsne(activations, TO_PLOT, PERPLEXITY, TSNE_ITER)
+    print("Generating image grid (%dx%d, %d images)" % (out_dim[0], out_dim[1], len(records)))
+    grid = calc_tsne_grid(X_2d, out_dim)
+
+    try:
+        # w, h = 530, 1000
+        w, h = ORIGINAL_IMAGE_SIZE[0] * CELL_RATIOS[0], ORIGINAL_IMAGE_SIZE[1] * CELL_RATIOS[1]
+        dim = max(w, h)
+        res = (int(SIDE*w/dim), int(SIDE*h/dim))
+        offset = (0, lambda x, _: PADDING * (x%2))
+        padding = (0, PADDING)
+        image, info = create_tsne_image(grid, records, out_dim, res, offset, padding)
+        print('Image:', image.shape, image.dtype)
+        image = Image.fromarray(image)
+        print('Image:', image.size, image.mode, image.format)
+        image.save('tsne.png', format='PNG', compress_level=9)
+        print("Creating tiles.")
+        create_tiles('4d2c04b0-51b7-4aa2-a234-0e4be53447de/0', image)
+        print('Saved image to tsne.png')
+        print(info)
+
+    except Exception as e:
+        print("Error generating image:", e)
+        raise
+
+if __name__ == "__main__":
+    main()
+
+
 # create_tiles(filename, image, out_dim, (side, side), info, current_set)
---
+++ poc/upload-sample-to-db.py
@@ -0,0 +1,49 @@
+import requests
+import os
+import sys
+from pathlib import Path
+import yaml
+from PIL import Image
+from io import BytesIO
+from itertools import chain
+
+screenshot_handler = 'https://screenshot-handler-qjzuw7ypfq-ez.a.run.app'
+item_crud_handler = 'https://chronomaps-api-qjzuw7ypfq-ez.a.run.app'
+
+if __name__ == '__main__':
+    root = Path(sys.argv[1])
+
+    for path, dirnames, filenames in root.walk():
+        config = path / '.chronomaps.config'
+        if not config.exists():
+            continue
+        config = yaml.safe_load(config.read_text())
+        workspace = config['workspace']
+        api_key = config['api_key']    
+        filenames = list(chain(path.glob('*.png'), path.glob('*.jpeg'), path.glob('*.jpg')))
+
+        print(f'Deleting items in workspace: {workspace}')
+        resp = requests.delete(f'{item_crud_handler}/{workspace}/items', headers={'Authorization': f'{api_key}'})
+        resp.raise_for_status()
+
+        for i, filename in enumerate(filenames):
+            print(f'Uploading image {filename} ({i+1}/{len(filenames)}) to workspace: {workspace}')
+            image = Image.open(filename)
+            image = image.convert('RGB')
+            image.thumbnail((2120, 4000), Image.Resampling.LANCZOS)
+            out = BytesIO()
+            image.save(out, format='jpeg', quality=85)
+            out.seek(0)
+            response = requests.post(
+                screenshot_handler,
+                files={
+                    'image': (filename.name, out, 'image/jpeg')
+                },
+                params={'workspace': workspace, 'api_key': api_key, 'automatic': 'true'},
+            )
+            if response.status_code == 200:
+                response = response.json()
+                item_id = response.get('item_id')
+                item_key = response.get('item_key')          
+            else:
+                print(f"Failed to upload file {i+1}: {filename}, Status Code: {response.status_code}, Response: {response.text}")
