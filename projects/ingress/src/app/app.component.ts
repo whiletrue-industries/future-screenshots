b2fa039e16c0c2ab2f77f437a8ea9fab7723b304
Adam Kariv <adam.kariv@gmail.com>
2025-05-04 14:10:26 +0300

    Link to privacy policy

--- projects/ingress/src/app/app.component.ts
+++ projects/ingress/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlatformService } from '../../platform.service';
+import { marked } from 'marked';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,20 @@ export class AppComponent {
   
   constructor(private platform: PlatformService) {
     this.platform.browser(() => {
+      const renderer = new marked.Renderer();
+      const linkRenderer = renderer.link;
+      renderer.link = (href: string, title: string, text: string) => {
+        const localLink = href.startsWith(`${location.protocol}//${location.hostname}`);
+        const html = linkRenderer.call(renderer, href, title, text);
+        return localLink ? html : html.replace(/^<a /, `<a target="_blank" rel="noreferrer noopener nofollow" `);  
+      };
+      marked.use({renderer});
       try {
         const orientation: any = window.screen.orientation;
         if (screen && orientation?.lock) {
-          // orientation?.lock('portrait').then(() => {
-          //   console.log('Screen orientation locked to portrait');
-          // });
+          orientation?.lock('portrait').then(() => {
+            console.log('Screen orientation locked to portrait');
+          });
         }          
       } catch (e) {
         console.error('Error locking screen orientation', e);
