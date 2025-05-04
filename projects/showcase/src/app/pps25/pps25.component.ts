71d31c07ea16ee0c76d1bb0fbc489f268ab7ad5e
Adam Kariv <adam.kariv@gmail.com>
2025-05-04 14:03:43 +0300

    Missing files

---
+++ projects/showcase/src/app/pps25/pps25.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { OutputMapComponent } from "../output-map/output-map.component";
+
+@Component({
+  selector: 'app-pps25',
+  imports: [OutputMapComponent],
+  templateUrl: './pps25.component.html',
+  styleUrl: './pps25.component.less'
+})
+export class Pps25Component {
+
+}
