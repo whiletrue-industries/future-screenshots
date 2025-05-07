5f709e9a47df24189f4adb47ca6c6c0b316acfa8
Adam Kariv <adam.kariv@gmail.com>
2025-05-07 12:17:27 +0300

    Missing i18n

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ mv dist/ingress/browser/en _dist && \
 mv dist/ingress/browser/he _dist && \
 mv dist/showcase/browser/en _dist/show && \
 mv dist/showcase/browser/he _dist/show && \
-mkdir _dist/show/pps25 && cp -r _dist/show/he/index.html _dist/show/pps25 && \
+mkdir _dist/show/pps25 && cp -r _dist/show/he/index.html _dist/show/he/pps25 && \
 (cp CNAME _dist/ || true) && \
 git add _dist && \
 git commit -m dist && \
