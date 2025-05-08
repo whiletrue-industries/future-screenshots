0fb5ee8acaf50a07715ebad5dbde11c2a39ac682
Adam Kariv <adam.kariv@gmail.com>
2025-05-08 16:06:03 +0300

    wide

---
+++ projects/showcase/src/app/pps25v/pps25.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { OutputMapComponent } from "../output-map/output-map.component";
+
+@Component({
+  selector: 'app-pps25v',
+  imports: [OutputMapComponent],
+  templateUrl: './pps25.component.html',
+  styleUrl: './pps25.component.less'
+})
+export class Pps25VComponent {
+
+}
