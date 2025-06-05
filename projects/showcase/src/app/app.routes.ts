61c37bc5fd63086bcd5c15cf9182a28ac569f26d
Adam Kariv <adam.kariv@gmail.com>
2025-06-05 19:05:04 +0300

    +

--- projects/showcase/src/app/app.routes.ts
+++ projects/showcase/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { OutputMapComponent } from './output-map/output-map.component';
 import { Pps25Component } from './pps25/pps25.component';
 import { Pps25VComponent } from './pps25v/pps25.component';
+import { Jma25Component } from './jma25/jma25.component';
 
 export const routes: Routes = [
     { path: 'pps25w', component: Pps25Component },
