eeaadc1b1bf6e49e74555b5b1b1b960e3ae865de
Adam Kariv <adam.kariv@gmail.com>
2025-06-05 19:01:13 +0300

    Add jma25 show path

--- projects/showcase/src/app/app.routes.ts
+++ projects/showcase/src/app/app.routes.ts
@@ -6,5 +6,6 @@ import { Pps25VComponent } from './pps25v/pps25.component';
 export const routes: Routes = [
     { path: 'pps25w', component: Pps25Component },
     { path: 'pps25', component: Pps25VComponent },
+    { path: 'jma25', component: Jma25Component },
     { path: '', component: OutputMapComponent }
 ];
---
+++ projects/showcase/src/app/jma25/jma25.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { OutputMapComponent } from "../output-map/output-map.component";
+
+@Component({
+  selector: 'app-jma25',
+  imports: [OutputMapComponent],
+  templateUrl: './jma25.component.html',
+  styleUrl: './jma25.component.less'
+})
+export class Jma25Component {
+
+}
