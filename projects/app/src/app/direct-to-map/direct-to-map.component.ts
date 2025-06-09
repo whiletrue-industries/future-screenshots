6769a1a883b0951211f9b7af6779175005a5885d
Adam Kariv <adam.kariv@gmail.com>
2025-06-09 22:49:34 +0300

    Complete flow

--- projects/app/src/app/direct-to-map/direct-to-map.component.ts
+++ projects/app/src/app/direct-to-map/direct-to-map.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-direct-to-map',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './direct-to-map.component.html',
   styleUrl: './direct-to-map.component.less'
 })
