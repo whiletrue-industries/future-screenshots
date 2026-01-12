86fa06c529b1b6fc22366123cf66e25bd99f1dc9
Mushon Zer-Aviv <mushon@shual.com>
2026-01-12 18:02:48 +0200

    Refactor code structure for improved readability and maintainability

--- projects/app/src/app/app.config.server.ts
+++ projects/app/src/app/app.config.server.ts
@@ -1,20 +1,10 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { Auth } from '@angular/fire/auth';
-import { FirebaseApp } from '@angular/fire/app';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    {
-      provide: FirebaseApp,
-      useValue: null
-    },
-    {
-      provide: Auth,
-      useValue: null
-    }
   ]
 };
 
